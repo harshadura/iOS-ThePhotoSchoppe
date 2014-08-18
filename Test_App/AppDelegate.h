1b565a4e9a4822f1662533ef3cdbc13edfd3bef0
Harsha Siriwardena <Harsha@Prabodhas-Mac-mini.local>
2014-08-18 17:33:01 +0530

    Initial Commit

---
+++ Test_App/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Test_App
+//
+//  Created by Harsha Siriwardena on 8/18/14.
+//  Copyright (c) 2014 Harsha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Test_App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test_App
+//
+//  Created by Harsha Siriwardena on 8/18/14.
+//  Copyright (c) 2014 Harsha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
